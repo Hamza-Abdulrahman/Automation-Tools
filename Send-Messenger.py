67d0434292829e12ebd483dbcb23df8ade8caf90
Hamza-Abdulrahman <75262372+Hamza-Abdulrahman@users.noreply.github.com>
2021-02-01 14:33:17 +0200

    send messenger massege for python

---
+++ Send-Messenger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+
+import fbchat
+
+from fbchat import Client
+
+from fbchat.models import *
+
+account = fbchat.Client('<Your Email>' , '<Your Password>')
+
+account.sendMessage(message='Masseges',thread_id='<id target>',thread_type=ThreadType.USER)
